5727524be50c204a0e31c2d0c363508e0b12ef4b
Max Andreas <mas09@hampshire.edu>
2018-02-27 22:51:56 -0700

    I was too excited to remember to commit until now

--- js/app.js
+++ js/app.js
@@ -1,14 +1,19 @@
-/*
- * Create a list that holds all of your cards
- */
+//list of cards
+const cardList = ['fa-diamond', 'fa-diamond', 'fa-paper-plane-o', 'fa-paper-plane-o', 'fa-anchor', 'fa-anchor', 'fa-bolt', 'fa-bolt', 'fa-cube', 'fa-cube', 'fa-leaf', 'fa-leaf', 'fa-bicycle', 'fa-bicycle', 'fa-bomb', 'fa-bomb'];
+//list of shuffled cards
+const cardsShuffled = shuffle(cardList);
+//select list items with card class
+const classItems = document.body.querySelectorAll('.card');
+//iterate through shuffled cards and set to innerhtml of each card class element
 
-
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
+//TODO increase performance by creating li as new elements, setting their class,
+// then updating innerhtml of the deck. Also make as function so it can be called for reset game
+// NEED to make sure if same card is clicked twice it doesn't respond!!
+let index = 0;
+for (let card of cardsShuffled) {
+  classItems[index].innerHTML = '<i class="fa ' + card + '"></i>';
+  index++;
+}
 
 // Shuffle function from http://stackoverflow.com/a/2450976
 function shuffle(array) {
@@ -25,14 +30,95 @@ function shuffle(array) {
     return array;
 }
 
+const restart = document.body.querySelector('.restart');
+restart.addEventListener('click', restartGame);
 
+const cardDeck = document.body.querySelector('.deck');
+cardDeck.addEventListener('click', cardClicked);
+
+function cardClicked(event) {
+  if (event.target.nodeName === 'LI' && waiting === false){
+    displayCard(event);
+    addToList(event);
+  }
+}
+
+function displayCard(event) {
+  event.target.className = 'card open show';
+}
+
+let openList = [];
+//variable to see if setTimout is in process on the card compariso to prevent more cards being opened
+let waiting = false;
+
+function addToList(event) {
+  openList.push(event.target);
+  if (openList.length === 2) {
+    if (openList[0] !== openList[1]) {
+      cardCompare();
+      moveCounter();
+    }
+    else {
+      openList.splice(0, 1);
+    }
+  }
+}
+
+function cardCompare() {
+  let card1 = openList[0].firstChild.className;
+  let card2 = openList[1].firstChild.className;
+  if (card1 === card2) {
+    keepCardsOpen();
+  } else {
+    waiting = true;
+    setTimeout(closeCards, 1200);
+  }
+}
+
+let wonGameCount = 0;
+
+function keepCardsOpen() {
+  openList[0].className = 'card match show';
+  openList[1].className = 'card match show';
+  wonGameCount++;
+  didIWin();
+  openList.splice(0, 2);
+}
+
+function closeCards() {
+  let this1 = openList[0];
+  let this2 = openList[1];
+  this1.className = 'card';
+  this2.className = 'card';
+  openList.splice(0, 2);
+  waiting = false;
+}
+
+
+let moves = 0;
+
+function moveCounter() {
+  moves++;
+  document.body.querySelector('.moves').textContent = moves;
+}
+
+function didIWin() {
+  if (wonGameCount === 8)
+  alert("You won the game!");
+}
+
+function restartGame() {
+  alert('this');
+}
 /*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+
+ * done- set up the event listener for a card. If a card is clicked:
+ * done- display the card's symbol (put this functionality in another function that you call from this one)
+ * done- add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ * done- if the list already has another card, check to see if the two cards match
+ * done- if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ * done- if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ * done- increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ * maybe+ if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+
  */
