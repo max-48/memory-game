587f01446b79c1b88b04113ffc1c9fa245815ee2
Max Andreas <mas09@hampshire.edu>
2018-06-27 18:20:12 -0600

    Returning to project after hiatus. Review files and add documetation

--- js/app.js
+++ js/app.js
@@ -9,11 +9,11 @@ startGame();
 
 //function run when page is opened to set up game
 function startGame() {
-  //list of shuffled cards
+  //pass cardList to shuffle function and return list of shuffled cards
   const cardsShuffled = shuffle(cardList);
-  //select list items with card class
+  //select list items with card class from deck
   const classItems = document.body.querySelectorAll('.card');
-  //iterate through shuffled cards and set to innerhtml of each card class element
+  //iterate through list of shuffled cards and set each card as innerhtml of each card class element
   let index = 0;
   for (let card of cardsShuffled) {
     classItems[index].className = 'card';
@@ -40,22 +40,34 @@ function shuffle(array) {
     return array;
 }
 
+// listener function for card deck being clicked. Checks to make sure an actual
+// card was the click target and that the click timeout for card comparison isn't running. Also
+// starts game timer if it is the first card click of the game, invokes function
+// to display the clicked card, and invokes function to begin card comparison process (addToList)
 function cardClicked(event) {
   if (event.target.nodeName === 'LI' && waiting === false){
+    if (firstClick === true){
+      startTimer();
+    }
     displayCard(event);
     addToList(event);
   }
 }
 
+//changes class of clicked card to make it "open" and display symbol
 function displayCard(event) {
   event.target.className = 'card open show';
 }
 
-let openList = [];
-
-//variable to see if setTimout is in process on the card compariso to prevent more cards being opened
+//variable to see if setTimout is in process on the card comparison to prevent more cards being opened
 let waiting = false;
 
+let openList = [];
+
+
+//Add click target to open card list. Checks to see if two cards have been added to open card list. If two cards,
+//make sure they are not the same target card (remove a card on list if same target),
+//invoke card comparison function, and add a move to the move counter
 function addToList(event) {
   openList.push(event.target);
   if (openList.length === 2) {
@@ -69,6 +81,9 @@ function addToList(event) {
   }
 }
 
+//Compares the two cards on the openList to see if they have same class name.
+//If identical, keep them open, if not, prevent other cards from being clicked (waiting=true)
+//and set timer to close cards after brief period
 function cardCompare() {
   let card1 = openList[0].firstChild.className;
   let card2 = openList[1].firstChild.className;
@@ -80,8 +95,11 @@ function cardCompare() {
   }
 }
 
+//card match counter. Indicates game is won when reaching "8"
 let wonGameCount = 0;
 
+//change class of "open" cards when matched, add one to match counter,
+//invoke function to see if all matches have been made, remove open cards from list
 function keepCardsOpen() {
   openList[0].className = 'card match show';
   openList[1].className = 'card match show';
@@ -90,6 +108,8 @@ function keepCardsOpen() {
   openList.splice(0, 2);
 }
 
+//change class of "open" cards to "close" when not matched, remove from openList,
+//and change waiting to false to allow new cards to be clicked
 function closeCards() {
   let this1 = openList[0];
   let this2 = openList[1];
@@ -99,23 +119,51 @@ function closeCards() {
   waiting = false;
 }
 
-
+//move counter variable
 let moves = 0;
 
+//adds 1 to move counter every time a new card click occurs
 function moveCounter() {
   moves++;
   document.body.querySelector('.moves').textContent = moves;
 }
 
+//checks to see if all cards have been matched
 function didIWin() {
   if (wonGameCount === 8)
   alert("You won the game!");
 }
 
+//game timer variable
+let gameTimer = 0;
+
+//function invoked to add a second to game timer
+function addToTimer() {
+  gameTimer++;
+  document.body.querySelector('.timer').textContent = gameTimer;
+}
+
+//variable to indicate if any cards have been clicked in current game
+let firstClick = true;
+//declare timer variable
+let myTimer = null;
+
+//invoked upon first card click to set timer interval
+function startTimer() {
+  myTimer = setInterval(addToTimer, 1000);
+  firstClick = false;
+}
+
+//when restart game icon is clicked, all variable are reset, timer restarted,
+//and start game function invoked
 function restartGame() {
   openList = [];
   waiting = false;
+  firstClick = true;
   wonGameCount = 0;
+  gameTimer = -1;
+  addToTimer();
+  clearInterval(myTimer);
   moves = -1;
   moveCounter();
   startGame();
